6fa24680c1b75b7b2e9f39c9d3eca4a7c994099e
Gheysiell <gheysiellfarukzinn@hotmail.com>
2024-08-18 22:10:32 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# math_operations
+
+<div> <h2> Mobile app made in Kotlin for sample of mathematical operations </h2> </div>
+<div> <h5> ° Open in Android studio and run the project </h5> </div>
---
+++ app/src/main/java/com/example/kotlin_first_app/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.math_operations
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var btnAdd: Button
+    private lateinit var btnSub: Button
+    private lateinit var btnMultiply: Button
+    private lateinit var btnDivision: Button
+    private lateinit var etA: EditText
+    private lateinit var etB: EditText
+    private lateinit var resultTv: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_subtraction)
+        btnMultiply = findViewById(R.id.btn_multiplication)
+        btnDivision = findViewById(R.id.btn_division)
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+        resultTv = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMultiply.setOnClickListener(this)
+        btnDivision.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val textA = etA.text.toString()
+        val textB = etB.text.toString()
+
+        if (textA.isEmpty() || textB.isEmpty()) {
+            Toast.makeText(this, "Por favor, digite um número em ambos os campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val a = textA.toDouble()
+        val b = textB.toDouble()
+        var result = 0.0
+
+        when(p0?.id) {
+            R.id.btn_add -> {
+                result = a + b
+            }
+            R.id.btn_subtraction -> {
+                result = a - b
+            }
+            R.id.btn_multiplication -> {
+                result = a * b
+            }
+            R.id.btn_division -> {
+                result = a / b
+            }
+        }
+        "Resultado: $result".also { resultTv.text = it }
+    }
+}
