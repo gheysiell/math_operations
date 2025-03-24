4eae4d2c77984d96221077041c568794d61ca08c
gheysiell <gheysiellfarukzinn@hotmail.com>
2025-03-23 22:53:56 -0400

    update README.md

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # math_operations
 
+| ![Home](https://raw.githubusercontent.com/gheysiell/images/main/math_operations.png) | !
+|:---:|:---:|
+
 <div> <h2> Mobile app made in Kotlin for sample of mathematical operations </h2> </div>
 <div> <h5> ° Open in Android studio and run the project </h5> </div>
--- app/src/main/java/com/example/math_operations/MainActivity.kt
+++ app/src/main/java/com/example/math_operations/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        window.statusBarColor = getColor(R.color.colorPrimary)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
